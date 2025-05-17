868248cf1aeca3864d8aa5485ffef33743759e38
Benjamin <aeplibenjamin@gmail.com>
2025-05-17 06:59:00 +0200

    finish up login visually and add authentication service

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -1,24 +1,52 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/authentication-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-passwordType: string = 'password';
-eyeIcon: string = 'fa-eye-slash';
+  passwordType: string = 'password';
+  eyeIcon: string = 'fa-eye-slash';
+  showLoginForm: boolean = true;
+  loginData = { username: '', password: '' };
+  registerData = { username: '', email: '', password: '', confirmPassword: '' };
 
-showLoginForm: boolean = true;
+  constructor(private authService: AuthService, private router: Router) {}
 
-toggleLoginAndRegisterForm(){
-  this.showLoginForm = !this.showLoginForm;
-}
+  toggleLoginAndRegisterForm() {
+    this.showLoginForm = !this.showLoginForm;
+  }
 
+  login() {
+    this.authService.login(this.loginData).subscribe({
+      next: (response) => {
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/home']);
+      },
+      error: (error) => {
+        console.error('Login failed', error);
+      },
+    });
+  }
+
+  register(){
+    this.authService.register(this.registerData).subscribe({
+      next: (response) => {
+        console.log('Registration successful', response);
+        this.toggleLoginAndRegisterForm();
+      },
+      error: (error) => {
+        console.error('Registration failed', error);
+      },
+    })
+  }
 }
 
 
